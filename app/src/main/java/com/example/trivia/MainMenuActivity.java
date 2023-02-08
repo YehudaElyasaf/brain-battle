5665930e8d791a2821e5cba4c0d82c90c9f4a8a0
Yehuda Elyasaf <hvusvvnkl12345@gmail.com>
2023-02-09 00:24:29 +0200

    refactor

--- app/src/main/java/com/example/trivia/MainMenuActivity.java
+++ app/src/main/java/com/example/trivia/MainMenuActivity.java
@@ -2,21 +2,42 @@ package com.example.trivia;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.GestureDetector;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.widget.AdapterView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class MainMenuActivity extends AppCompatActivity {
-    BottomNavigationView navigationView;
-    StartGameFragment startGameFragment;
-    ScoreFragment scoreFragment;
+enum DifficultyLevel{
+    ALL,
+    EASY,
+    MEDIUM,
+    HARD
+}
+enum Category{
+    ALL,
+    GENERAL_KNOWLEDGE,
+    HISTORY,
+    SCIENCE,
+    PROGRAMMING
+}
+
+public class MainMenuActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener {
+    public static final int MIN_SWIPE_LENGTH = 100;
+
+    private GestureDetectorCompat swipeDetector;
+    private BottomNavigationView navigationView;
+    private StartGameFragment startGameFragment;
+    private ScoreFragment scoreFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +47,7 @@ public class MainMenuActivity extends AppCompatActivity {
         navigationView = findViewById(R.id.mainNavigationView);
         startGameFragment = new StartGameFragment();
         scoreFragment = new ScoreFragment();
+        swipeDetector = new GestureDetectorCompat(this, this);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.mainFragmentContainer, startGameFragment).commit();
         navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -46,5 +68,61 @@ public class MainMenuActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+    @Override
+    public boolean onDown(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(@NonNull MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(@NonNull MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.mainFragmentContainer);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if(Math.abs(e2.getX() - e1.getX()) > MIN_SWIPE_LENGTH){
+            //swipe
+            if(e2.getX() > e1.getX()){
+                //right swipe
+                if(currentFragment instanceof ScoreFragment){
+                    fragmentTransaction.replace(R.id.mainFragmentContainer, startGameFragment).commit();
+                    navigationView.setSelectedItemId(R.id.startGameFragment);
+                }
+            }
+            else{
+                //left swipe
+                if(currentFragment instanceof StartGameFragment){
+                    fragmentTransaction.replace(R.id.mainFragmentContainer, scoreFragment).commit();
+                    navigationView.setSelectedItemId(R.id.scoreFragment);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        swipeDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
--- app/src/main/java/com/example/trivia/User.java
+++ app/src/main/java/com/example/trivia/User.java
@@ -4,7 +4,6 @@ public class User {
     private int id;
     private String username;
     private String password;
-    private int coins;
     private int totalCorrect;
     private int totalWrong;
 }
